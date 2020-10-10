d074f3594f7cbdada38c9676f765d9e5b14e17e1
Daniel Larsen <dalars@microsoft.com>
2020-10-11 12:27:13 +1300

    Fix deploy-sprocs

--- deploy/deploy-sprocs.ps1
+++ deploy/deploy-sprocs.ps1
@@ -1,15 +1,20 @@
-Install-Module -Name CosmosDB   # Thanks @PlagueHO !
+# Deploys any stored procedures found in /sprocs
+
+$ErrorActionPreference = 'Stop'
+
+Install-Module -Name CosmosDB -Confirm   # Thanks @PlagueHO !
 
 $cosmos = 'numberservice'
 $cosmosdb = 'NumberService'
 $collection = 'Numbers'
 
-$primaryKey = ConvertTo-SecureString -String $env:COSMOSPRIMARYMASTERKEY -AsPlainText -Force
+$cosmosKey = ( az cosmosdb keys list -n $cosmos -g $rg --type 'keys' | ConvertFrom-Json ).primaryMasterKey
+$primaryKey = ConvertTo-SecureString -String $cosmosKey -AsPlainText -Force
 $cosmosDbContext = New-CosmosDbContext -Account $cosmos -Database $cosmosdb -Key $primaryKey
 
 $sprocs = Get-CosmosDbStoredProcedure -Context $cosmosDbContext -CollectionId $collection | % { $_.Id }
 
-Get-ChildItem -Path ./cosmos/sprocs -Name | ForEach-Object {
+Get-ChildItem -Path ../sprocs -Name | ForEach-Object {
     [string] $body = Get-Content -Path $( Join-Path '../sprocs' $_ ) -Raw
     $id = $_.Replace('.js', '')
     if ($sprocs -contains $id) {
