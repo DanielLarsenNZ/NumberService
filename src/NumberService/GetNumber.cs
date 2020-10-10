84e7c659de540249c0e8f889f0d13b0a975c27c8
Daniel Larsen <dalars@microsoft.com>
2020-10-10 23:26:43 +1300

    Calling sproc

--- src/NumberService/GetNumber.cs
+++ src/NumberService/GetNumber.cs
@@ -1,35 +1,36 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace NumberService
 {
     public static class GetNumber
     {
-        private static HttpClient _http = new HttpClient();
-        private static long _lastNumber = 10000;     //TODO: App Setting
-        private static string _storageUrl = "http://localhost:7071/api/numbers";       //TODO: App Setting
-        private static string _session = Guid.NewGuid().ToString("N");
+        private static readonly string _clientId = Guid.NewGuid().ToString("N");
+        private static readonly Container _container =
+            new CosmosClient(Environment.GetEnvironmentVariable("CosmosDbConnectionString"))
+            .GetContainer(
+                Environment.GetEnvironmentVariable("CosmosDbDatabaseId"),
+                Environment.GetEnvironmentVariable("CosmosDbContainerId"));
 
         [FunctionName("GetNumber")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "numbers")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var number = await _container.Scripts.ExecuteStoredProcedureAsync<NumberResult>(
+                "incrementNumber",
+                new PartitionKey("number3"),
+                new dynamic[] { "number3", _clientId });
 
-            Interlocked.Increment(ref _lastNumber);
+            log.LogInformation($"Number {number.Resource.Number} issued to clientId {number.Resource.ClientId} with ETag {number.Resource.ETag} from key {number.Resource.Key}");
 
-            var result = await _http.GetFromJsonAsync<NumberResult>($"{_storageUrl}/{_lastNumber}?session={_session}");
-
-            return new OkObjectResult(result);
+            return new OkObjectResult(number.Resource);
         }
     }
 }
--- src/NumberService/NumberResult.cs
+++ src/NumberService/NumberResult.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
 
 namespace NumberService
 {
     public class NumberResult
     {
+        [JsonProperty("id")]
+        public string Key { get; set; }
+
         public long Number { get; set; }
+
+        public string ClientId { get; set; }
+        
+        [JsonProperty("_etag")]
+        public string ETag { get; set; }
     }
 }
