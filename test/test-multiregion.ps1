3fc033957d02341d45545b1ef46fd1e0a637c4c9
Daniel Larsen <dalars@microsoft.com>
2021-03-21 15:24:55 +1300

    #7 a better multi-region test

--- test/test-multiregion.ps1
+++ test/test-multiregion.ps1
@@ -1,13 +1,41 @@
+$numbers = [System.Collections.Concurrent.ConcurrentDictionary[string,object]]::new()
+
+$i = 0
 while ($true) {
-    $response = $null
-    $response = Invoke-RestMethod -Method Put -Uri 'https://numberservice-aue.azurewebsites.net/api/numbers/free' 
+    $i++
+    'https://numberservice-aue.azurewebsites.net/api/numbers/free?diagnostics', 'https://numberservice-ase.azurewebsites.net/api/numbers/free?diagnostics' | ForEach-Object -Parallel {
+
+        $response = $null
+        $response = Invoke-RestMethod -Method Put -Uri $_ 
+        Write-Host $response.number -ForegroundColor Yellow
+        Write-Host $response
+        Write-Host ( $response.CosmosDiagnostics.Context | Where-Object -Property Id -eq 'StoreResponseStatistics' )
+
+        $numbers = $using:numbers
+        $numbers[$_] = $response.number
+    }
+
+    if ($numbers['https://numberservice-aue.azurewebsites.net/api/numbers/free?diagnostics'] -eq $numbers['https://numberservice-ase.azurewebsites.net/api/numbers/free?diagnostics']) {
+        Write-Host "COLLISION after $i tests" -ForegroundColor Red
+        Write-Host $numbers['https://numberservice-aue.azurewebsites.net/api/numbers/free?diagnostics'] -ForegroundColor Yellow
+        Write-Host $numbers['https://numberservice-ase.azurewebsites.net/api/numbers/free?diagnostics'] -ForegroundColor Yellow
+        break
+    }
+}
+
+<#
+while ($true) {
     
+    $response = $null
+    $response = Invoke-RestMethod -Method Put -Uri 'https://numberservice-aue.azurewebsites.net/api/numbers/free?diagnostics' 
     Write-Host $response
+    Write-Host ( $response.CosmosDiagnostics.Context | Where-Object -Property Id -eq 'StoreResponseStatistics' )
 
     $response = $null
-    $response = Invoke-RestMethod -Method Put -Uri 'https://numberservice-ase.azurewebsites.net/api/numbers/free' 
-    
+    $response = Invoke-RestMethod -Method Put -Uri 'https://numberservice-ase.azurewebsites.net/api/numbers/free?diagnostics' 
     Write-Host $response
+    Write-Host ( $response.CosmosDiagnostics.Context | Where-Object -Property Id -eq 'StoreResponseStatistics' )
 
     #Start-Sleep -Seconds 5
 }
+#>
